9cf5b473c9562b3deaaa4f0d4a55aed8f65be9c7
timo <timo@renner.solutions>
2020-02-11 10:06:15 +0000

    removed CMakeFiles

--- CMakeFiles/homegear.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/homegear"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/homegear.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- CMakeFiles/homegear_loxone_miniserver.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libhomegear_loxone_miniserver.a"
-)
