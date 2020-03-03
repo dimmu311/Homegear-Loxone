22cbbc7463e5d5f1620ddaec34f62400867dc485
timo <timo@renner.solutions>
2020-03-03 20:54:57 +0000

    correctet some paths

--- makeCoverity.sh
+++ makeCoverity.sh
@@ -13,7 +13,7 @@ rm -Rf autom4te.cache
 ./bootstrap || exit 1
 ./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --libdir=/usr/lib || exit 1
 cov-build --dir cov-int make -j${BUILDTHREADS}
-tar -czvf homegear-velux-klf200.tgz cov-int
+tar -czvf homegear-loxone-miniserver.tgz cov-int
 rm -Rf cov-int
 make clean
 make distclean
