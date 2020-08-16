816788ce3b6f1f71f4af3afb85b20d90005d5d7e
root <root@Server.localdomain>
2020-08-16 16:41:27 +0200

    changes to compile as .so and added some controls

---
+++ src/controls/Jalousie.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Admin on 16.08.2020.
+//
+
+#include "Jalousie.h"
---
+++ src/controls/Jalousie.h
@@ -0,0 +1,14 @@
+//
+// Created by Admin on 16.08.2020.
+//
+
+#ifndef LOXONE_MINISERVER_JALOUSIE_H
+#define LOXONE_MINISERVER_JALOUSIE_H
+
+
+class Jalousie {
+
+};
+
+
+#endif //LOXONE_MINISERVER_JALOUSIE_H
