3bfa1316516467256b2bbd8f99d6346f85a91cd9
Timo <timo@renner.solutions>
2021-09-01 08:18:04 +0200

    added NFCCodeTouch

--- src/controls/createInstance.cpp
+++ src/controls/createInstance.cpp
@@ -5,6 +5,7 @@
 #include "ColorPicker.h"
 #include "Central.h"
 #include "Daytimer.h"
+#include "NFCCodeTouch.h"
 
 namespace Loxone
 {
@@ -46,7 +47,7 @@ namespace Loxone
         else if(type == "CentralLightController") return std::make_shared<Central>(control, room, cat, 0x504);
         else if(type == "Intercom") return std::make_shared<LoxoneControl>(control, room, cat, 0x505);
         else if(type == "IntercomV2") return std::make_shared<LoxoneControl>(control, room, cat, 0x506);
-        else if(type == "NFCCodeTouch") return std::make_shared<LoxoneControl>(control, room, cat, 0x507);
+        else if(type == "NFCCodeTouch") return std::make_shared<NFCCodeTouch>(control, room, cat);
         else return std::make_shared<LoxoneControl>(control, room, cat, 0x001);
         //Alarm Sequence, this in in Doku, but not shown in struct file. don't know if this is a Loxone issu
 
@@ -87,7 +88,7 @@ namespace Loxone
         else if(typeNr == 0x504) return std::make_shared<Central>(rows, typeNr); //CentralLightController
         else if(typeNr == 0x505) return std::make_shared<LoxoneControl>(rows, typeNr); //Intercom
         else if(typeNr == 0x506) return std::make_shared<LoxoneControl>(rows, typeNr); //IntercomV2
-        else if(typeNr == 0x507) return std::make_shared<LoxoneControl>(rows, typeNr); //NFC Code Touch
+        else if(typeNr == 0x507) return std::make_shared<NFCCodeTouch>(rows, typeNr); //NFC Code Touch
         else return std::make_shared<LoxoneControl>(rows, typeNr);
         //Alarm Sequence, this in in Doku, but not shown in struct file. don't know if this is a Loxone issu
 
